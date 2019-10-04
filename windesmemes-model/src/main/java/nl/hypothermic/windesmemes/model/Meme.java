49abec57ccac496287ef3c19c3221183152d643b
hypothermic <admin@hypothermic.nl>
2019-10-04 10:13:25 +0200

    Add utility methods to data beans
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- windesmemes-model/src/main/java/nl/hypothermic/windesmemes/model/Meme.java
+++ windesmemes-model/src/main/java/nl/hypothermic/windesmemes/model/Meme.java
@@ -2,6 +2,8 @@ package nl.hypothermic.windesmemes.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 public class Meme {
 
     @SerializedName("id")
@@ -52,5 +54,26 @@ public class Meme {
 
     public int parseVote() {
         return Integer.valueOf(vote);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Meme meme = (Meme) o;
+        return id == meme.id &&
+                userId == meme.userId &&
+                Objects.equals(username, meme.username) &&
+                Objects.equals(karma, meme.karma) &&
+                Objects.equals(vote, meme.vote) &&
+                Objects.equals(flair, meme.flair) &&
+                Objects.equals(imageUrl, meme.imageUrl) &&
+                Objects.equals(title, meme.title) &&
+                Objects.equals(date, meme.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, userId, karma, vote, flair, imageUrl, title, date);
     }
 }
--- windesmemes-model/src/main/java/nl/hypothermic/windesmemes/model/User.java
+++ windesmemes-model/src/main/java/nl/hypothermic/windesmemes/model/User.java
@@ -2,6 +2,8 @@ package nl.hypothermic.windesmemes.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 public class User {
 
     @SerializedName("user_id")
@@ -34,4 +36,25 @@ public class User {
     @SerializedName("owner")
     public boolean owner;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId &&
+                userLevel == user.userLevel &&
+                createdMemes == user.createdMemes &&
+                totalKarma == user.totalKarma &&
+                owner == user.owner &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(avatar_id, user.avatar_id) &&
+                Objects.equals(biography, user.biography) &&
+                Objects.equals(member_since, user.member_since) &&
+                Objects.equals(flair, user.flair);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, userLevel, avatar_id, biography, member_since, flair, createdMemes, totalKarma, owner);
+    }
 }
