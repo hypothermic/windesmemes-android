ff8cbd619a9e6084830930474412dcc0e58c4e2a
hypothermic <admin@hypothermic.nl>
2019-10-06 17:14:27 +0200

    Clear MemeCachedAttributes if database is too large
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- app/src/main/java/nl/hypothermic/windesmemes/android/data/persistance/CachedAttributesDatabase.java
+++ app/src/main/java/nl/hypothermic/windesmemes/android/data/persistance/CachedAttributesDatabase.java
@@ -6,12 +6,18 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.io.File;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
+import nl.hypothermic.windesmemes.android.LogWrapper;
+
 @Database(entities = { MemeCachedAttributes.class }, version = 4, exportSchema = false)
 public abstract class CachedAttributesDatabase extends RoomDatabase {
+
+    private static final String DATABASE_PATH = "cache.db";
+    private static final long SIZE_LIMIT = 64L * 1000 * 1000; // = 64MB, TODO let user specify
 
     public static final Executor IO_THREAD = Executors.newSingleThreadExecutor(new ThreadFactory() {
         @Override
@@ -23,12 +29,24 @@ public abstract class CachedAttributesDatabase extends RoomDatabase {
     private static CachedAttributesDatabase INSTANCE;
     private static final Object LOCK = new Object();
 
-    public static CachedAttributesDatabase getInstance(Context context) {
+    public static CachedAttributesDatabase getInstance(final Context context) {
         synchronized (LOCK) {
             if (INSTANCE == null) {
-                INSTANCE = Room.databaseBuilder(context, CachedAttributesDatabase.class, "cache.db")
+                INSTANCE = Room.databaseBuilder(context, CachedAttributesDatabase.class, DATABASE_PATH)
                                    .fallbackToDestructiveMigration()
                                    .build();
+
+                IO_THREAD.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        File database = context.getDatabasePath(DATABASE_PATH);
+                        long size = database.length();
+                        if (database.exists() && size > SIZE_LIMIT) {
+                            LogWrapper.error(INSTANCE, "Clearing all tables because database was over size (%d bytes)", size);
+                            INSTANCE.clearAllTables();
+                        }
+                    }
+                });
             }
             return INSTANCE;
         }
